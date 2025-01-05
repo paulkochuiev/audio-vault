ab22d6eaaf866049aa969d310dfc1fac3ab372ae
Pavlo Kochuiev <pavlo.kochuev@artpix3d.com>
2025-01-05 01:13:43 +0100

    feat: add footer and header

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,3 +1,3 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Audio Vault";
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "AudioVault";
 export const APP_DESC = process.env.NEXT_PUBLIC_APP_DESC || "E-commerce platform for music equipment distribution";
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
