74c1adddce118479e513eefc0d422ca0c43650b1
Paul Kochuiev <pavlo.kochuev@artpix3d.com>
2025-01-05 15:08:36 +0100

    feat: add themes logic to app

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,29 +1,47 @@
+"use client";
+
 import { ShoppingCart, UserIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 
 import { APP_NAME } from "@/lib/constants";
 import { Button } from "@/components/ui/button";
+import ModeToggle from "./mode-toggle";
+import { useTheme } from "next-themes";
 
 const Header = () => {
+	const { resolvedTheme } = useTheme();
+
+	const logoSrc =
+		resolvedTheme === "light" ? "/images/logoBlack.png" : "/images/logoWhite.png";
+
 	return (
 		<header>
 			<div className="w-full border-b">
 				<div className="wrapper flex-between">
 					<div className="flex-start">
 						<Link href="/" className="flex-start">
-							<Image src="/images/logo.png" alt={`${APP_NAME} logo`} height={48} width={48} priority />
-							<span className="hidden lg:block font-bold text-2xl ml-3">{APP_NAME}</span>
+							<Image
+								src={logoSrc}
+								alt={`${APP_NAME} logo`}
+								height={48}
+								width={48}
+								priority
+							/>
+							<span className="hidden lg:block font-bold text-2xl ml-3">
+								{APP_NAME}
+							</span>
 						</Link>
 					</div>
 					<div className="space-x-2">
+						<ModeToggle />
 						<Button asChild variant="ghost">
 							<Link href="/cart">
 								<ShoppingCart />
 								Cart
 							</Link>
 						</Button>
-						<Button asChild variant="ghost">
+						<Button asChild>
 							<Link href="/sign-in">
 								<UserIcon />
 								Sign In
