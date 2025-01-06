11481f93373a32fcebf565eff9f479e539527447
Paul Kochuiev <paulkochuiev@gmail.com>
2025-01-06 15:58:04 +0100

    feat: add sampleData and product-list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,15 @@ export const metadata = {
 	title: "Home",
 };
 
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const Homepage = () => {
-	return <>AudioVault</>;
+	return (
+		<>
+			<ProductList data={sampleData.products} title="Newest Arrivals" limit={4} />
+		</>
+	);
 };
 
 export default Homepage;
