2b0c348997fe5c54d7a41df248f013cff06abf65
Paul Kochuiev <paulkochuiev@gmail.com>
2025-09-16 23:13:27 +0200

    feat: add product-price component

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import Image from "next/image";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import ProductPrice from "./product-price";
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
-    <Link href={`/product/${product.slug}`}>
-      <Card className="w-full max-w-sm">
+    <Card className="w-full max-w-sm">
+      <Link href={`/product/${product.slug}`}>
         <CardHeader className="p-0 items-center">
           <Image
             src={product.images[0]}
@@ -18,17 +19,17 @@ const ProductCard = ({ product }: { product: any }) => {
         <CardContent className="p-4 grid gap-4">
           <div className="text-xs">{product.brand}</div>
           <h2 className="text-sm font-medium">{product.name}</h2>
-          <div className="felx-between gap-4">
+          <div className="flex-between gap-4">
             <p>{product.rating} Stars</p>
             {product.stock > 0 ? (
-              <p className="font-bold">{product.price}</p>
+              <ProductPrice value={Number(product.price)} />
             ) : (
               <p className="text-destructive">Out Of Stock</p>
             )}
           </div>
         </CardContent>
-      </Card>
-    </Link>
+      </Link>
+    </Card>
   );
 };
 
