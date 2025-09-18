b7d03a3ad5eca1a0779b2328a553d247c01deefa
Paul Kochuiev <paulkochuiev@gmail.com>
2025-09-18 20:08:23 +0200

    feat: install zod on app

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,9 @@ export const cn = (...inputs: ClassValue[]) => {
 export const convertToPlainObject = <T>(value: T): T => {
   return JSON.parse(JSON.stringify(value));
 };
+
+export const formatNumberWithDecimal = (num: number): string => {
+  const [int, decimal] = num.toString().split(".");
+
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
+};
