8cc33f6bc7bd61bce16b891f649e512a489d45ba
Paul Kochuiev <paulkochuiev@gmail.com>
2025-09-26 14:43:03 +0200

    feat: add add-to-cart and toast components

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,30 +4,37 @@ import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
 import { APP_DESC, APP_NAME, SERVER_URL } from "@/lib/constants";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: {
-		template: `%s | ${APP_NAME}`,
-		default: APP_NAME,
-	},
-	description: APP_DESC,
-	metadataBase: new URL(SERVER_URL),
+  title: {
+    template: `%s | ${APP_NAME}`,
+    default: APP_NAME,
+  },
+  description: APP_DESC,
+  metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body className={`${inter.className} antialiased`}>
-				<ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
-					{children}
-				</ThemeProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
