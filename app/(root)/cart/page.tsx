660df4d68b23a84f3f0653a536d3e0475077bfe0
Paul Kochuiev <paulkochuiev@gmail.com>
2025-09-26 18:20:12 +0200

    feat: add cart-table

---
+++ app/(root)/cart/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next";
+import CartTable from "./cart-table";
+import { getMyCart } from "@/lib/actions/cart.actions";
+
+export const metadata: Metadata = {
+  title: "Shopping Cart",
+};
+
+const CartPage = async () => {
+  const cart = await getMyCart();
+
+  return (
+    <>
+      <CartTable cart={cart} />
+    </>
+  );
+};
+
+export default CartPage;
