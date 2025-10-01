5c2a9e97a2825cc8776abf9a6d4863fda1b37eb3
Paul Kochuiev <paulkochuiev@gmail.com>
2025-10-01 12:24:31 +0200

    feat: add createOrder to paypal

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,6 +1,45 @@
 const base = process.env.PAYPAL_API_URL || "https://api-m.sandbox.paypal.com";
 
-export const paypal = {};
+export const paypal = {
+  createOrder: async function createOrder(price: number) {
+    const accessToken = await generateAccessToken();
+
+    const url = `${base}/v2/checkout/orders`;
+
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        intent: "CAPTURE",
+        purchase_units: [
+          {
+            amount: { currency_code: "USD" },
+            value: price,
+          },
+        ],
+      }),
+    });
+
+    return handleResponse(res);
+  },
+  capturePayment: async function capturePayment(orderId: string) {
+    const accessToken = await generateAccessToken();
+    const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+
+    const res = await fetch(url, {
+      method: "POSR",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return handleResponse(res);
+  },
+};
 
 const generateAccessToken = async () => {
   const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
@@ -18,10 +57,14 @@ const generateAccessToken = async () => {
     },
   });
 
-  if (res.ok) {
-    const jsonData = await res.json();
+  const jsonData = await handleResponse(res);
 
-    return jsonData.access_token;
+  return jsonData.access_token;
+};
+
+const handleResponse = async (res: Response) => {
+  if (res.ok) {
+    return await res.json();
   } else {
     const errorMessage = await res.text();
 
