c02c88797b4a484a7006a40e5e87158c374d9ff3
Paul Kochuiev <paulkochuiev@gmail.com>
2025-10-15 22:31:36 +0200

    feat: add callbackUrl

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -15,8 +15,8 @@ export const ourFileRouter = {
 
       return { userId: session?.user?.id };
     })
-    .onUploadComplete(async ({ metadata }) => {
-      return { uploadedBy: metadata.userId };
+    .onUploadComplete(async ({ metadata, file }) => {
+      return { uploadedBy: metadata.userId, url: file.url };
     }),
 } satisfies FileRouter;
 export type OurFileRouter = typeof ourFileRouter;
--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -3,4 +3,5 @@ import { ourFileRouter } from "./core";
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
+  config: { callbackUrl: process.env.UPLOADTHING_CALLBACK_URL },
 });
