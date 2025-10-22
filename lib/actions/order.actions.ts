224ac233e8f7ac5116145d17ce6dab8a2217d34d
Paul Kochuiev <paulkochuiev@gmail.com>
2025-10-23 00:28:13 +0200

    feat: add resend email

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -7,11 +7,12 @@ import { auth } from "@/auth";
 import { getMyCart } from "./cart.actions";
 import { getUserById } from "./user.actions";
 import { insertOrderSchema } from "../validators";
-import { CartItem, PaymentResult } from "@/types";
+import { CartItem, PaymentResult, ShippingAddress } from "@/types";
 import { paypal } from "../paypal";
 import { revalidatePath } from "next/cache";
 import { PAGE_SIZE } from "../constants";
 import { Prisma } from "@prisma/client";
+import { sendPurchaseReceipt } from "@/email";
 
 export const createOrder = async () => {
   try {
@@ -248,6 +249,14 @@ export const updateOrderToPaid = async ({
   if (!updatedOrder) {
     throw new Error("Order not found");
   }
+
+  sendPurchaseReceipt({
+    order: {
+      ...updatedOrder,
+      shippingAddress: updatedOrder.shippingAddress as ShippingAddress,
+      paymentResult: updatedOrder.paymentResult as PaymentResult,
+    },
+  });
 };
 
 export const getMyOrders = async ({
